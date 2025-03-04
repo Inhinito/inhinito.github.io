6f6e7821ced03baacb7cb1a54504c2c489b14c6e
Nikitas I/O <chrononautic@gmail.com>
2025-03-04 10:58:32 +0200

    Improved projects

---
+++ js/landing/projects.js
@@ -0,0 +1,234 @@
+// The array of project objects.
+const projects = [
+  {
+    id: 1,
+    title: {
+        english: "Article Reader AI",
+        greek: "Article Reader AI"
+    },
+    type: [
+      {
+        english: "Sofware",
+        greek: "Λογισμικό"
+      }
+    ],
+    subtype: [
+      {
+        english: "Mobile Apps",
+        greek: "Εφαρμογές"
+      }
+    ],
+    client: {
+        english: "Ideas Forward",
+        greek: "Ideas Forward"
+    },
+    image: "media/images/projects/article-reader.png",
+    link: "https://articlereader.ai/",
+  },
+  {
+    id: 2,
+    title: {
+        english: "Eptapyrgio Videography",
+        greek: "Βιντεοσκόπηση Επταπυργίου"
+    },
+    type: [
+      {
+        english: "Multimedia",
+        greek: "Πολυμέσα"
+      }
+    ],
+    subtype: [
+      {
+        english: "Videography",
+        greek: "Βιντεοσκόπηση"
+      }
+    ],
+    client: {
+        english: "Cultural Center of Central Macedonia",
+        greek: "Κέντρο Πολιτισμού Kεντρικής Μακεδονίας"
+    },
+    image: "/api/placeholder/200/200",
+    link: "https://example.com/",
+  },
+  {
+    id: 3,
+    title: {
+        english: "Walk Advertisement",
+        greek: "Walk Διαφήμιση",
+    },
+    type: [
+      {
+        english: "Multimedia",
+        greek: "Πολυμέσα"
+      }, {
+        english: "Marketing",
+        greek: "Μάρκετινγκ"
+      }
+    ],
+    subtype: [
+      {
+        english: "Videography",
+        greek: "Βιντεοσκόπηση"
+      }, {
+        english: "Digital Ads",
+        greek: "Διαφημίσεις"
+      }
+    ],
+    client: {
+        english: "Aristotle University",
+        greek: "Αριστοτέλειο Πανεπιστήμιο"
+    },
+    image: "media/images/projects/walk.svg",
+    link: "https://example.org/",
+  },
+  {
+    id: 4,
+    title: {
+        english: "Project Title",
+        greek: "Τίτλος Έργου"
+    },
+    type: [
+      {
+        english: "Sofware",
+        greek: "Λογισμικό"
+      }
+    ],
+    subtype: [
+      {
+        english: "Mobile App",
+        greek: "Εφαρμογή"
+      }
+    ],
+    client: {
+        english: "Client Name",
+        greek: "Όνομα Πελάτη"
+    },
+    image: "/api/placeholder/200/300",
+    link: "https://example.net/",
+  },
+  {
+    id: 5,
+    title: {
+        english: "Project Title",
+        greek: "Τίτλος Έργου"
+    },
+    type: [
+      {
+        english: "Sofware",
+        greek: "Λογισμικό"
+      }
+    ],
+    subtype: [
+      {
+        english: "Mobile App",
+        greek: "Εφαρμογή"
+      }
+    ],
+    client: {
+        english: "Client Name",
+        greek: "Όνομα Πελάτη"
+    },
+    image: "/api/placeholder/300/300",
+    link: "https://example.io/",
+  },
+  {
+    id: 6,
+    title: {
+        english: "Project Title",
+        greek: "Τίτλος Έργου"
+    },
+    type: [
+      {
+        english: "Sofware",
+        greek: "Λογισμικό"
+      }
+    ],
+    subtype: [
+      {
+        english: "Mobile App",
+        greek: "Εφαρμογή"
+      }
+    ],
+    client: {
+        english: "Client Name",
+        greek: "Όνομα Πελάτη"
+    },
+    image: "/api/placeholder/400/200",
+    link: "https://example.co/",
+  },
+  {
+    id: 7,
+    title: {
+        english: "Project Title",
+        greek: "Τίτλος Έργου"
+    },
+    type: [
+      {
+        english: "Sofware",
+        greek: "Λογισμικό"
+      }
+    ],
+    subtype: [
+      {
+        english: "Mobile App",
+        greek: "Εφαρμογή"
+      }
+    ],
+    client: {
+        english: "Client Name",
+        greek: "Όνομα Πελάτη"
+    },
+    image: "/api/placeholder/300/200",
+    link: "https://example.dev/",
+  },
+  {
+    id: 8,
+    title: {
+        english: "Project Title",
+        greek: "Τίτλος Έργου"
+    },
+    type: [
+      {
+        english: "Sofware",
+        greek: "Λογισμικό"
+      }
+    ],
+    subtype: [
+      {
+        english: "Mobile App",
+        greek: "Εφαρμογή"
+      }
+    ],
+    client: {
+        english: "Client Name",
+        greek: "Όνομα Πελάτη"
+    },
+    image: "/api/placeholder/200/300",
+    link: "https://example.app/",
+  },
+  {
+    id: 9,
+    title: {
+        english: "Project Title",
+        greek: "Τίτλος Έργου"
+    },
+    type: [
+      {
+        english: "Sofware",
+        greek: "Λογισμικό"
+      }
+    ],
+    subtype: [
+      {
+        english: "Mobile App",
+        greek: "Εφαρμογή"
+      }
+    ],
+    client: {
+        english: "Client Name",
+        greek: "Όνομα Πελάτη"
+    },
+    image: "/api/placeholder/200/200",
+    link: "https://example.org/",
+  },
+];
--- js/landing/signal.js
+++ js/landing/signal.js
@@ -1,184 +1,3 @@
-// The array of project objects.
-const projects = [
-  {
-    id: 1,
-    title: "Article Reader AI",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile Apps",
-        greek: "Εφαρμογές"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "media/images/projects/article-reader.png",
-    link: "https://articlereader.ai/",
-  },
-  {
-    id: 2,
-    title: "Walk Advertisement",
-    type: [
-      {
-        english: "Multimedia",
-        greek: "Πολυμέσα"
-      }, {
-        english: "Marketing",
-        greek: "Μάρκετινγκ"
-      }
-    ],
-    subtype: [
-      {
-        english: "Videography",
-        greek: "Βιντεοσκόπηση"
-      }, {
-        english: "Digital Ads",
-        greek: "Διαφημίσεις"
-      }
-    ],
-    client: "ΑΠΘ (AUTh)",
-    image: "media/images/projects/walk.svg",
-    link: "https://example.org/",
-  },
-  {
-    id: 3,
-    title: "Yet another app",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile App",
-        greek: "Εφαρμογή"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "/api/placeholder/200/200",
-    link: "https://example.com/",
-  },
-  {
-    id: 4,
-    title: "TRADER APP",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile App",
-        greek: "Εφαρμογή"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "/api/placeholder/200/300",
-    link: "https://example.net/",
-  },
-  {
-    id: 5,
-    title: "ANOTHER APP",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile App",
-        greek: "Εφαρμογή"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "/api/placeholder/300/300",
-    link: "https://example.io/",
-  },
-  {
-    id: 6,
-    title: "Lolipop",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile App",
-        greek: "Εφαρμογή"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "/api/placeholder/400/200",
-    link: "https://example.co/",
-  },
-  {
-    id: 7,
-    title: "ANOTHER APP",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile App",
-        greek: "Εφαρμογή"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "/api/placeholder/300/200",
-    link: "https://example.dev/",
-  },
-  {
-    id: 8,
-    title: "Chocolate",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile App",
-        greek: "Εφαρμογή"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "/api/placeholder/200/300",
-    link: "https://example.app/",
-  },
-  {
-    id: 9,
-    title: "ANOTHER APP",
-    type: [
-      {
-        english: "Sofware",
-        greek: "Λογισμικό"
-      }
-    ],
-    subtype: [
-      {
-        english: "Mobile App",
-        greek: "Εφαρμογή"
-      }
-    ],
-    client: "Ideas Forward",
-    image: "/api/placeholder/200/200",
-    link: "https://example.org/",
-  },
-];
-
 // Constants
 const numPulses = projects.length + 1;
 const pulseHeight = 400;
@@ -351,7 +170,7 @@ function createProjects(container, language) {
     imageFrame.className = "pixel-image-frame";
     const imgEl = document.createElement("img");
     imgEl.src = project.image;
-    imgEl.alt = project.title;
+    imgEl.alt = project.title[language];
     imgEl.className = "pixel-image";
     imageFrame.appendChild(imgEl);
 
@@ -375,13 +194,15 @@ function createProjects(container, language) {
     // Title
     const titleEl = document.createElement("h3");
     titleEl.className = "list-header article-header";
-    titleEl.textContent = project.title;
+    titleEl.textContent = project.title[language];
 
     // Client with dot
     const clientEl = document.createElement("div");
     clientEl.className = "pixel-description";
     clientEl.appendChild(document.createElement("span")).className = "pixel-blink-dot";
-    clientEl.appendChild(document.createElement("span")).textContent = project.client;
+    clientEl.appendChild(document.createElement("span")).textContent = project.client[language];
+    // Add a class to the text content.
+    clientEl.lastElementChild.className = "pixel-client";
 
 
     // Button
