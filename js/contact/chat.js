afd1f5a8ad15b49025ad45a76295c1fe355aaa98
Nikitas I/O <chrononautic@gmail.com>
2025-06-03 18:27:06 +0300

    Added thumbnail metas

--- js/contact/chat.js
+++ js/contact/chat.js
@@ -2,17 +2,23 @@
 var chatbox = document.getElementById('fb-customer-chat');
 chatbox.setAttribute("page_id", "175067943434194");
 chatbox.setAttribute("attribution", "biz_inbox");
+
 window.fbAsyncInit = function() {
     FB.init({
-    xfbml            : true,
-    version          : 'v10.0'
+        appId      : '549522668554179',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v17.0'
     });
+        
+    FB.AppEvents.logPageView();    
 };
 
-(function(d, s, id) {
+(function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
+    if (d.getElementById(id)) {return;}
     js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+// Load the Facebook SDK asynchronously
