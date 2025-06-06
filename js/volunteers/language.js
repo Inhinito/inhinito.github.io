baac6ffc7ce275b5c8f414c905b01c91650a3106
Nikitas I/O <chrononautic@gmail.com>
2025-06-06 14:38:24 +0300

    Fixed footer issues

--- js/volunteers/language.js
+++ js/volunteers/language.js
@@ -5,6 +5,7 @@ const volunteersEnglish = document.getElementById('volunteers-english');
 const volunteersGreek = document.getElementById('volunteers-greek');
 
 const mainContent = document.querySelector('main');
+const clientHeaders = document.querySelector('.client-headers');
 
 mainContent.style.display = 'block';
 fadeInElement(mainContent, 500);
@@ -13,8 +14,11 @@ english.addEventListener('click', function(e){
   e.preventDefault();
   // Set the language flag to English.
   languageFlag='english';
+
+  console.log('The client headers are: ', clientHeaders);
   
   mainContent.style.opacity = 0;
+  clientHeaders.style.opacity = 0;
   footerDetails.style.opacity = 0;
   
   // Wait half a second for the menu to fade out.
@@ -22,6 +26,7 @@ english.addEventListener('click', function(e){
     volunteersGreek.style.display = 'none';
 
     mainContent.style.opacity = 1;
+    clientHeaders.style.opacity = 1;
     volunteersEnglish.style.display = 'block';
 
     // Change the language content of the footer.
@@ -38,6 +43,7 @@ greek.addEventListener('click', function(e){
   languageFlag='greek';
 
   mainContent.style.opacity = 0;
+  clientHeaders.style.opacity = 0;
   footerDetails.style.opacity = 0;
 
   // Wait half a second for the menu to fade out.
@@ -46,6 +52,7 @@ greek.addEventListener('click', function(e){
     volunteersEnglish.style.display = 'none';
 
     mainContent.style.opacity = 1;
+    clientHeaders.style.opacity = 1;
     volunteersGreek.style.display = 'block';
 
     // Change the language content of the footer.
